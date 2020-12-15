742c4608bd332af299f5025fa4a8db0569fe8db8
Tin Martinic <neviox@users.noreply.github.com>
2020-12-15 19:34:05 +0100

    .

--- vjezba5/dictionary.c
+++ vjezba5/dictionary.c
@@ -166,5 +166,126 @@ void dodaj_na_pocetak_liste(Dictionary dict,char* str){
   }
   
 }
-  
+void add_word(Dictionary dict, char *str){
+  Dictionary n = dict->next;
+  Dictionary nova = (Dictionary)malloc(sizeof(Word));
+  nova->word = (char*)malloc(sizeof(char) * strlen(str) + 1); 
+  strcpy(nova->word, str);
+  nova->count = 1;
+  nova->next = dict->next;
+  dict->next = nova;
+}
+char* longest_word(Dictionary dict){
+    dict=dict->next;
+    char* temp=dict->word;
+    while(dict!=NULL){
+        if(strlen(dict->word)>strlen(temp)){
+            temp=dict->word;
+        }
+    dict=dict->next;
+    }
+  return temp;
+}
+int word_sum(Dictionary dict){
+    int wordsum=0;
+    while(dict!=NULL){
+        wordsum+=dict->count;
+        dict=dict->next;
+    }
+    return wordsum;
+}
+void delete_before_element(Dictionary dict, char* str){
+    Dictionary novi=(Dictionary)malloc(sizeof(Word));
+    novi->word=(char*)malloc(sizeof(char)*strlen(str)+1);
+    novi->count=1;
+    char test[]="youngest";
+    strcpy(novi->word,str);
+    Dictionary n=dict->next;
+    Dictionary p=dict;
+    while(n!=NULL){
+        if(strcmp(n->word,test)==0){
+            novi->next=n;
+            dict->next=novi;
+        }
+    n=n->next;
+    dict=dict->next;
+    }
+}
+void insert(Dictionary dict,char* str,int index){
+    Dictionary novi=(Dictionary)malloc(sizeof(Word));
+    novi->word=(char*)malloc(sizeof(char)*strlen(str)+1);
+    novi->count=1;
+    strcpy(novi->word,str);
+    Dictionary n=dict->next;
+    int brojac=0;
+    while(n!=NULL){
+        if(brojac==index){
+            novi->next=n;
+            dict->next=novi;
+        }
+    brojac++;
+    n=n->next;
+    dict=dict->next;
+    }
+}
+void delete_first(Dictionary dict){
+    Dictionary temp=dict->next;
+    Dictionary n=dict->next->next;
+    dict->next=n;
+    free(temp->word);
+    free(temp);
+}
+// void delete_last(Dictionary dict){
+//     Dictionary n=dict->next;
+// do{
+//     n=dict;
+//     dict=dict->next;
+//     if(dict->next==NULL){
+//         n->next=NULL;
+//         free(dict->next);
+//         }
+//     }
+//     while(n->next!=NULL);
+// };
+void delete_word (Dictionary dict, char* str){
+    Dictionary p=dict;
+    dict=dict->next;
+    Dictionary n=dict->next;
+    while (n!=NULL){
+        if (strcmp(n->word,str)==0){
+            p->next=p->next->next;
+            free(n->word);
+            
+        }
+    p=p->next;
+    n=n->next;
+    }
+}
+void brisi_zadnji(Dictionary dict){
+    dict=dict->next;
+    Dictionary n=dict->next;
+    Dictionary p=dict;
+    while(n->next!=NULL){
+        n=n->next;
+        p=p->next;
+    }
+    p->next=NULL;
+    free(n->word);
+    free(n);
+}
+ 
+void okreni_listu(Dictionary dict) {
+  Dictionary p = dict->next;
+  Dictionary n = dict->next->next;
+  Dictionary tmp = n;
+  p->next = NULL;
+  while(tmp != NULL) {
+    tmp = tmp->next;
+    n->next = p;
+    p = n;
+    n = tmp;
+  }
+  dict->next = p;
+
+}
 
